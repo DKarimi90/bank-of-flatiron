31fa0c72602f414fbb5215385e90baadba47ae80
david <davmwa90@gmail.com>
2023-01-17 14:45:15 +0300

    Defined the required functions

--- src/Component/TransactionForm.js
+++ src/Component/TransactionForm.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, {useState}from 'react'
 
 function TransactionForm() {
+    const [transactionList, setTransactionList] = useState()
+    const [date, setDate] = useState("")
+    const [description, setDescription] = useState([])
+    const [category, setCategory] = useState([])
+    const [amount, setAmount] = useState([])
+
+
+    function handleChange() {
+        setDate(date)
+    }
+
+    function handleChange() {
+        setDescription(description)
+    }
+
+    function handleChange() {
+        setCategory(category)
+    }
+
+    function handleChange() {
+        setAmount(amount)
+    }
+    function handleClick() {
+    }
+
+
   return (
     <div>
         <form className = "addTransaction">
@@ -9,6 +35,7 @@ function TransactionForm() {
                 <input type = "text" name ="description" />
                 <input type = "text" name ="category" />
                 <input type = "amount" name ="amount" />
+                <button onClick = {handleClick} >Update</button>
             </div>
 
         </form>
